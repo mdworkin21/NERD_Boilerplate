4a97ec983f46613143db88218ecc3cad90ce2a5d
Matt Dworkin <mdworkin21@gmail.com>
2018-09-19 14:58:01 -0400

    basic boiler setup

--- client/index.js
+++ client/index.js
@@ -5,12 +5,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Homepage from './components/Homepage'
 import {Provider} from 'react-redux'
-import store from './store'
-
+// import store from './store'
 // import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider>
     <Homepage />
   </Provider>, 
   document.getElementById('root'))
+
+  //Uncomment line below when you want to try to implement service workers (for PWA)
+  // registerServiceWorker()
+
+
+  //In provider tag, put store={store} (pass it the prop), once store is set up and uncomment import statement
